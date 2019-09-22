35b631b183d29e518a4cf517bd232d7feac7ee0e
EileenJuergens <juergens.eileen@gmail.com>
2019-09-22 23:24:24 +0200

    add bare CSS and with material-ui

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,19 +1,34 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import './Dashboard.css'
+import './Dashboard.css';
+import Lottie from 'react-lottie';
+import animationData from '../assets/heart.json';
+
 
 const Dashboard = () => {
+
+  const defaultOptions = {
+    loop: true,
+    autoplay: true, 
+    path: 'heart.json',
+    animationData: animationData,
+  };
+
   return (
-    <div>
-      <h1>Do you have a wish?</h1>
-      <NavLink to="/WishList">
-        <button>My Wish List</button>
-      </NavLink>
-      <NavLink to="/Friends">
-        <button>My Friends</button>
-      </NavLink>
+    <div className="container-dashboard">
+      <h1 className="title-dashboard">Do you have a wish?</h1>
+      <Lottie options={defaultOptions} height={300} width={300}/>
+      <div className="buttons-container">
+        <NavLink to="/WishList">
+          <button className="btn-dashboard">My Wish List</button>
+        </NavLink>
+        <NavLink to="/Friends">
+          <button className="btn-dashboard">My Friends</button>
+        </NavLink>
+      </div>
     </div>
   );
 };
+
 
 export default Dashboard;
--- client/src/components/WishList.js
+++ client/src/components/WishList.js
@@ -1,43 +1,88 @@
 import React from "react";
+import './WishList.css'
 import { NavLink } from "react-router-dom";
 import { connect } from "react-redux";
+import Home from '../assets/home-icon.png';
+
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
 
 const WishList = ({ wishList }) => {
+  const classes = useStyles();
+
   return (
-    <div>
-      <h3>My Wish List</h3>
-      <NavLink to="/">
-        <button>Back to Dashboard</button>
-      </NavLink>
-      <NavLink to="/NewWishForm">
-        <button>Add a wish</button>
-      </NavLink>
-      <table>
-        <thead>
-          <tr>
-            <th>Description</th>
-            <th>Price</th>
-            <th>Details</th>
-          </tr>
-        </thead>
-        <tbody>
-          {wishList.map(wish => (
-            <tr key={wish.id}>
-              <td>{wish.description}</td>
-              <td>{wish.price}</td>
-              <td>
-                <NavLink to="/NewWishForm">
-                  <button>See Details</button>
-                </NavLink>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+    <div className="container-wishlist">
+      <h3 className="title-wishlist">My Wish List</h3>
+      <div className="add-btn-container">
+        <NavLink to="/NewWishForm">
+          <button className="btn-addwish">Add a wish</button>
+        </NavLink>
+      </div>
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell style={{ maxWidth: "2rem"}}>Description</TableCell>
+              <TableCell style={{ maxWidth: "2rem"}}>Price&nbsp;(€)</TableCell>
+              <TableCell style={{ maxWidth: "2rem"}}>Details</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {wishList.map(wish => (
+              <TableRow key={wish.id}>
+                <TableCell align="left">{wish.description}</TableCell>
+                <TableCell align="left">{wish.price}</TableCell>
+                <TableCell align="left">
+                  <NavLink to="/NewWishForm">
+                    <button>See Details</button>
+                  </NavLink>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </Paper>
+
+
+
+
+
+
+
+      <div className="footer">
+        <NavLink to="/">
+          <button className="btn-wishlist">Back</button>
+          {/* <div classname="home-icon" >
+            <img src={Home}/>
+          </div> */}
+        </NavLink>
+      </div>
     </div>
   );
 };
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing(3),
+    overflowX: 'auto',
+  },
+  table: {
+    minWidth: 650,
+  },
+}));
+
+function createData(description, price, details) {
+  return { description, price, details };
+}
+
+
 
 const mapStateToProps = state => ({
   wishList: state.wishList
